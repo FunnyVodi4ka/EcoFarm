73343e2489ec288fdea346dc78321e44e1fa5a03
FunnyVodi4ka <vodi4ka.note@yandex.ru>
2023-04-29 17:22:19 +0300

    Добавлена страница просмотра аквариумов, страница добавления\изменения аквариумов

--- EcoFarm/FishFarming/PageAddEditAquariums.xaml.cs
+++ EcoFarm/FishFarming/PageAddEditAquariums.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EcoFarm.AppConnection;
+using EcoFarm.Authentication;
+using EcoFarm.DatabaseConnection;
+using EcoFarm.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,185 @@ namespace EcoFarm.FishFarming
     /// </summary>
     public partial class PageAddEditAquariums : Page
     {
-        public PageAddEditAquariums()
+        AccessVerification access = new AccessVerification();
+        ValidationClass validation = new ValidationClass();
+        private Aquariums currentAquarium = new Aquariums();
+
+        public PageAddEditAquariums(Aquariums aquarium)
         {
+            access.CheckMenegerAccess();
+
             InitializeComponent();
+
+            SetFish();
+
+            if (aquarium != null)
+            {
+                currentAquarium = aquarium;
+
+                DisableAttributeEditField();
+                FindAquarium();
+            }
+            else
+            {
+                comboBoxFish.SelectedIndex = 0;
+                currentAquarium.BoardingDate = DateTime.Now;
+            }
+
+            DataContext = currentAquarium;
+        }
+
+        private void DisableAttributeEditField()
+        {
+            comboBoxFish.IsEnabled = false;
+            dpDate.IsEnabled = false;
+        }
+
+        private void SetFish()
+        {
+            comboBoxFish.Items.Add("Выберите вид рыбы");
+            foreach (var fish in AppConnect.ModelDB.Fish)
+            {
+                comboBoxFish.Items.Add(fish.Name);
+            }
+            comboBoxFish.SelectedIndex = 0;
+        }
+
+        private void FindAquarium()
+        {
+            var fish = AppConnect.ModelDB.Fish.FirstOrDefault(x => x.IdFish == currentAquarium.IdFish);
+            comboBoxFish.SelectedItem = fish.Name;
+        }
+
+        private bool CheckAllData()
+        {
+            tbNumber.BorderBrush = Brushes.Black;
+            dpDate.BorderBrush = Brushes.Black;
+            tbSize.BorderBrush = Brushes.Black;
+            tbNote.BorderBrush = Brushes.Black;
+
+            if (!validation.CheckUniqueFieldNumber(tbNumber.Text, currentAquarium.IdAquarium))
+            {
+                tbNumber.BorderBrush = Brushes.Red;
+                MessageBox.Show("Ошибка: Аквариум с таким номером уже используется!");
+                return false;
+            }
+            if (!validation.CheckStringData(tbNumber.Text, 1, 50))
+            {
+                tbNumber.BorderBrush = Brushes.Red;
+                MessageBox.Show("Ошибка: Номер не может содержать меньше 1 и больше 50 символов!");
+                return false;
+            }
+            if (!validation.CheckDoubleData(tbSize.Text))
+            {
+                tbSize.BorderBrush = Brushes.Red;
+                MessageBox.Show("Ошибка: Некорректный объём аквариума!");
+                return false;
+            }
+            if (!validation.CheckDate(dpDate.Text))
+            {
+                dpDate.BorderBrush = Brushes.Red;
+                MessageBox.Show("Ошибка: Некорректная дата выпуска рыбы!");
+                return false;
+            }
+            if (tbNote.Text.Length > 0)
+            {
+                if (!validation.CheckStringData(tbNote.Text, 2, 500))
+                {
+                    tbNote.BorderBrush = Brushes.Red;
+                    MessageBox.Show("Ошибка: Заметки не могут содержать меньше 2 и больше 500 символов!");
+                    return false;
+                }
+            }
+            if (!CheckFish())
+            {
+                MessageBox.Show("Выберите вид рыбы!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckFish()
+        {
+            var fish = AppConnect.ModelDB.Fish.FirstOrDefault(x => x.Name == comboBoxFish.SelectedItem.ToString());
+            if (fish != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void SetIdFish()
+        {
+            var fish = AppConnect.ModelDB.Fish.FirstOrDefault(x => x.Name == comboBoxFish.SelectedItem.ToString());
+            if (fish != null)
+            {
+                currentAquarium.IdFish = fish.IdFish;
+            }
+        }
+
+        private void tbSize_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(e.Text, "^[0-9.]"))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frameMain.GoBack();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckAllData())
+            {
+                try
+                {
+                    currentAquarium.Number = tbNumber.Text;
+                    SetIdFish();
+                    currentAquarium.BoardingDate = DateTime.Parse(dpDate.Text);
+                    currentAquarium.Size = double.Parse(tbSize.Text.Replace('.', ','));
+                    currentAquarium.Note = tbNote.Text;
+
+                    if (currentAquarium.IdAquarium == 0)
+                    {
+                        EcoFarmDBEntities.GetContext().Aquariums.Add(currentAquarium);
+                        SaveCurrentWork();
+                    }
+
+                    EcoFarmDBEntities.GetContext().SaveChanges();
+                    AppConnect.ModelDB.SaveChanges();
+                    MessageBox.Show("Данные успешно сохранены!", "Уведомление",
+                                MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    AppFrame.frameMain.GoBack();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show("Ошибка " + Ex.Message.ToString() + "Критическая работа приложения", "Уведомление",
+                                MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void SaveCurrentWork()
+        {
+            List<FishWork> works = AppConnect.ModelDB.FishWork.ToList();
+            works = works.Where(x => x.IdFish == currentAquarium.IdFish).ToList();
+
+            foreach (FishWork work in works)
+            {
+                CurrentWorksForFishFarming newCurrentWork = new CurrentWorksForFishFarming();
+
+                newCurrentWork.IdAquarium = currentAquarium.IdAquarium; 
+                newCurrentWork.IdWork = work.IdWork;
+                newCurrentWork.DateOfNextWork = currentAquarium.BoardingDate;
+
+                EcoFarmDBEntities.GetContext().CurrentWorksForFishFarming.Add(newCurrentWork);
+            }
         }
     }
 }
--- EcoFarm/FishFarming/PageAquariums.xaml.cs
+++ EcoFarm/FishFarming/PageAquariums.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EcoFarm.AppConnection;
+using EcoFarm.AppSupportClass;
+using EcoFarm.Authentication;
+using EcoFarm.CropProduction;
+using EcoFarm.DatabaseConnection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +25,156 @@ namespace EcoFarm.FishFarming
     /// </summary>
     public partial class PageAquariums : Page
     {
+        AccessVerification access = new AccessVerification();
+
         public PageAquariums()
         {
+            access.CheckMenegerAccess();
+
             InitializeComponent();
+
+            SetFilter();
+            SetSort();
+            ListAquariums.ItemsSource = SortFilterFields();
+        }
+
+        Aquariums[] SortFilterFields()
+        {
+            List<Aquariums> aquariums = AppConnect.ModelDB.Aquariums.ToList();
+            var CounterALL = aquariums;
+            if (textBoxSearch.Text != null)
+            {
+                aquariums = aquariums.Where(x => x.Number.ToLower().Contains(textBoxSearch.Text.ToLower())).ToList();
+
+                switch (comboBoxSort.SelectedIndex)
+                {
+                    case 1:
+                        aquariums = aquariums.OrderBy(x => x.Number).ToList();
+                        break;
+                    case 2:
+                        aquariums = aquariums.OrderByDescending(x => x.Number).ToList();
+                        break;
+                }
+            }
+
+            if (comboBoxFilter.SelectedIndex > 0)
+            {
+                aquariums = aquariums.Where(x => x.Fish.Name == comboBoxFilter.SelectedItem.ToString()).ToList();
+            }
+
+            if (aquariums.Count != 0)
+            {
+                Counter.Text = "Показано: " + aquariums.Count + " из " + CounterALL.Count;
+            }
+            else
+            {
+                Counter.Text = "Не найдено";
+            }
+
+            return aquariums.ToArray();
+        }
+
+        private void SetSort()
+        {
+            comboBoxSort.Items.Add("Без сортировки");
+            comboBoxSort.Items.Add("По алфавиту А-Я");
+            comboBoxSort.Items.Add("По алфавиту Я-А");
+
+            comboBoxSort.SelectedIndex = 0;
+        }
+
+        private void SetFilter()
+        {
+            comboBoxFilter.Items.Add("Все виды рыб");
+            foreach (var plants in AppConnect.ModelDB.Fish)
+            {
+                comboBoxFilter.Items.Add(plants.Name);
+            }
+
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppConnect.ModelDB.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+            ListAquariums.ItemsSource = SortFilterFields();
+        }
+
+        private void textBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ListAquariums.ItemsSource = SortFilterFields();
+        }
+
+        private void comboBoxFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListAquariums.ItemsSource = SortFilterFields();
+        }
+
+        private void comboBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListAquariums.ItemsSource = SortFilterFields();
+        }
+
+        private void ListAquariums_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Aquariums row = ListAquariums.SelectedItem as Aquariums;
+            AppFrame.frameMain.Navigate(new PageAddEditAquariums(row));
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageAddEditAquariums((sender as Button).DataContext as Aquariums));
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var currentRow = ListAquariums.SelectedItems.Cast<Aquariums>().ToList().ElementAt(0);
+            try
+            {
+                if (MessageBox.Show("Вы уверены, что хотите удалить запись?", "Предупреждение", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    AppConnect.ModelDB.Aquariums.Remove(currentRow);
+                    AppConnect.ModelDB.SaveChanges();
+                    ListAquariums.ItemsSource = SortFilterFields();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка удаления!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void TabBarTasksToday_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SelectedMenuTab.selectedMenuTab = "PageFishFarming";
+            AppFrame.frameMain.Navigate(new PageTasksTodayForFishFarming());
+        }
+
+        private void TabBarAquariums_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (access.CheckMenegerAccessBoolResult())
+            {
+                SelectedMenuTab.selectedMenuTab = "PageFishFarming";
+                AppFrame.frameMain.Navigate(new PageAquariums());
+            }
+        }
+
+        private void TabBarFish_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (access.CheckMenegerAccessBoolResult())
+            {
+                SelectedMenuTab.selectedMenuTab = "PageFishFarming";
+                AppFrame.frameMain.Navigate(new PageFish());
+            }
+        }
+
+        private void TabBarListOfWorks_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (access.CheckMenegerAccessBoolResult())
+            {
+                SelectedMenuTab.selectedMenuTab = "PageFishFarming";
+                AppFrame.frameMain.Navigate(new PageListOfWorksForFishFarming());
+            }
         }
     }
 }
