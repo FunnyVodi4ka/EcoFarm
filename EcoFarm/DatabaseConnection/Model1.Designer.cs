f2bb03e04f2be26ce2e6b8537aa55846ae580706
FunnyVodi4ka <AlexZuevOfficial@yandex.ru>
2023-05-17 14:19:37 +0300

    Тестирование

--- EcoFarm/DatabaseConnection/Model1.Designer.cs
+++ EcoFarm/DatabaseConnection/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\AlexZ\Source\Repos\FunnyVodi4ka\EcoFarm\EcoFarm\DatabaseConnection\Model1.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\AlexZ\source\repos\FunnyVodi4ka\EcoFarm\EcoFarm\DatabaseConnection\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
